e9973e437470544b8fbaeafcc30556e1785f708c
Jakub Jaskuła <jjcreator8@gmail.com>
2021-03-21 17:49:19 +0100

    added fullscreen mobile navbar, hamburger animations, changed navbar layout

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -1,62 +1,40 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { HashLink } from 'react-router-hash-link';
 import styles from '../modules/mobileNavbarStyle.module.css';
 
 const {
+  bar1,
+  bar2,
+  bar3,
+  hamburger,
+  hamburgerBar,
+  hamburgerWrapper,
   mobileNavigation,
-  mobileIcon,
   mobileNavbarBody,
   mobileExitIcon,
-  mobileHomeLinks,
   mobileHomeLink,
 } = styles;
 
 const MobileNavbar = () => {
-  const [isVisible, setIsVisible] = useState({
-    hamburger: {
-      display: 'block',
-    },
-    menu: {
-      transform: 'translateX(-100%)',
-    },
-  });
+  const [isExpanded, setIsExpanded] = useState(false);
 
-  const toggleMobileMenu = () => {
-    if (isVisible.hamburger.display === 'block') {
-      setIsVisible({
-        hamburger: {
-          display: 'none',
-        },
-        menu: {
-          transform: 'translateX(0)',
-        },
-      });
-    } else {
-      setIsVisible({
-        hamburger: {
-          display: 'block',
-        },
-        menu: {
-          transform: 'translateX(-100%)',
-        },
-      });
-    }
-  };
+  const toggleMobileMenu = () => setIsExpanded(!isExpanded);
 
   return (
     <div className={mobileNavigation}>
-      <i role="button" id={mobileIcon} className="fas fa-bars" style={isVisible.hamburger} onClick={toggleMobileMenu} tabIndex="0" label="mobile menu" />
-      <div className={mobileNavbarBody} style={isVisible.menu}>
-        <i role="button" tabIndex={-1} id={mobileExitIcon} onClick={toggleMobileMenu} className="fas fa-times" label="exit" />
-        <div className={`flexColumn ${mobileHomeLinks}`}>
-          <NavLink id={mobileHomeLink} exact to="/" onClick={toggleMobileMenu}>Home</NavLink>
-          <HashLink to="/#mission" onClick={toggleMobileMenu}>mission</HashLink>
-          <HashLink to="/#services" onClick={toggleMobileMenu}>services</HashLink>
-          <HashLink to="/#testimonials" onClick={toggleMobileMenu}>testimonials</HashLink>
+      <div className={hamburgerWrapper}>
+        <div className={hamburger} role="button" onClick={toggleMobileMenu} tabIndex="0" label="mobile menu">
+          <span className={hamburgerBar} id={bar1} style={isExpanded ? { transform: 'translate(0px, 8px) rotate(45deg)' } : undefined} />
+          <span className={hamburgerBar} id={bar2} style={isExpanded ? { transform: 'scale(0.2)' } : undefined} />
+          <span className={hamburgerBar} id={bar3} style={isExpanded ? { transform: 'translate(0px, -8px) rotate(-45deg)' } : undefined} />
         </div>
-        <NavLink to="/about" onClick={toggleMobileMenu}>About</NavLink>
-        <NavLink to="/contact" onClick={toggleMobileMenu}>Contact</NavLink>
+      </div>
+
+      <div className={mobileNavbarBody} style={isExpanded ? { transform: 'translateX(0)' } : { transform: 'translateX(-100%)' }}>
+        <i role="button" tabIndex={-1} id={mobileExitIcon} onClick={toggleMobileMenu} className="fas fa-times" label="exit" />
+        <NavLink id={mobileHomeLink} exact to="/" onClick={toggleMobileMenu}>Stronga główna</NavLink>
+        <NavLink to="/about" onClick={toggleMobileMenu}>O książce</NavLink>
+        <NavLink to="/contact" onClick={toggleMobileMenu}>Kim jestem?</NavLink>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,73 +1,31 @@
 /* eslint-disable max-len */
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { HashLink as Link } from 'react-router-hash-link';
-import PropTypes from 'prop-types';
 import styles from '../modules/navbarStyle.module.css';
 import logo from '../images/logo.svg';
 
 const {
-  flexColumn, myNavbar, logoImg, logoContainer, links, homeLinks, link, underline,
+  buyNowButton, buyNowButtonWrapper, flexColumn, myNavbar, logoImg, logoContainer, links, link, underline,
 } = styles;
 
-function Navbar({ setNavIsAvailable }) {
-  const [displayHomeMenu, setDisplayHomeMenu] = useState('none');
-  const toggleDisplayHomeMenu = () => {
-    if (displayHomeMenu === 'block') {
-      setDisplayHomeMenu('none');
-    } else setDisplayHomeMenu('block');
-  };
-
+function Navbar() {
   return (
     <nav
       className={`${myNavbar} ${flexColumn}`}
-      onClick={() => {
-        setNavIsAvailable(false);
-      }}
     >
       <div className={flexColumn} id={logoContainer}>
-        <img className={logoImg} src={logo} alt="logo" />
-      </div>
-      <div id={styles.wave}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path fill="rgb(1,25,54)" fillOpacity="1" d="M0,192L80,165.3C160,139,320,85,480,85.3C640,85,800,139,960,138.7C1120,139,1280,85,1360,58.7L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z" />
-        </svg>
+        <img className={logoImg} src={logo} alt="logo tenisa rytmicznego" />
       </div>
       <div className={flexColumn} id={links}>
-        <div className={`flexColumn ${homeLinks}`}>
-          <NavLink
-            onClick={() => {
-              window.scrollTo(0, 0);
-              toggleDisplayHomeMenu();
-            }}
-            className={link}
-            activeClassName={underline}
-            exact
-            to="/"
-          >
-            Home
-            {' '}
-            <i className="fa fa-caret-down" />
-          </NavLink>
-          <ul style={{ display: displayHomeMenu }}>
-            <Link to="/#mission"><li>mission</li></Link>
-            <Link to="/#services"><li>services</li></Link>
-            <Link to="/#testimonials"><li>testimonials</li></Link>
-          </ul>
-        </div>
-        <NavLink className={link} activeClassName={underline} to="/about" onClick={() => setDisplayHomeMenu('none')}>About</NavLink>
-        <NavLink className={link} activeClassName={underline} to="/contact" onClick={() => setDisplayHomeMenu('none')}>Contact</NavLink>
+        <NavLink className={link} activeClassName={underline} exact to="/">Strona główna</NavLink>
+        <NavLink className={link} activeClassName={underline} to="/about">O książce</NavLink>
+        <NavLink className={link} activeClassName={underline} to="/contact">Kim jestem?</NavLink>
+      </div>
+      <div className={buyNowButtonWrapper}>
+        <button type="button" className={buyNowButton}>Kup teraz</button>
       </div>
     </nav>
   );
 }
-
-Navbar.defaultProps = {
-  setNavIsAvailable: () => {},
-};
-
-Navbar.propTypes = {
-  setNavIsAvailable: PropTypes.func,
-};
 
 export default Navbar;
