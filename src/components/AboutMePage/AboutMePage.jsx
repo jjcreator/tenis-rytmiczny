9d963ac3dd28b7bbdf653d813a453fc1b995713a
Jakub Jaskuła <jjcreator8@gmail.com>
2021-04-09 18:17:26 +0200

    v1 created

---
+++ src/components/AboutMePage/AboutMePage.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from '../../modules/aboutMePage.module.css';
+import ContentPanel from '../Common/ContentPanel';
+import doctor from '../../images/doctor.jpg';
+import tennis from '../../images/tennis.jpg';
+import running from '../../images/running.jpg';
+import oranges from '../../images/oranges.jpg';
+import cycling from '../../images/cycling-mountains.jpg';
+import ImagePanel from './ImagePanel';
+import QuotePanel from './QuotePanel';
+
+const aboutMeText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+
+const aboutMeText2 = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum';
+
+const quote = 'Dziś jest pierwszy dzień z reszty twojego życia';
+const author = 'José Saramago';
+
+const images = [
+  { image: tennis, alt: 'tenis ziemny' },
+  { image: running, alt: 'bieganie' },
+  { image: oranges, alt: 'cytrusy' },
+  { image: cycling, alt: 'kolarstwo górskie' },
+];
+
+const {
+  aboutMe,
+  animated,
+} = styles;
+
+const AboutMe = () => (
+  <div className={`flexColumn ${animated} ${aboutMe}`}>
+    <ContentPanel title="O mnie" heading="Dr Franciszek Jan Walczak" text={aboutMeText} text2={aboutMeText2} img={doctor} alt="moje zdjęcie" imageIsVertical />
+    <ImagePanel images={images} />
+    <QuotePanel quote={quote} author={author} />
+  </div>
+);
+
+export default AboutMe;
---
+++ src/components/Common/ContentPanel.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from '../../modules/contentPanel.module.css';
+
+const {
+  contentPanel, contentPanelHeading, contentPanelTitle, contentPanelBody, contentPanelText, contentPanelTextTitle, contentPanelImage, contentPanelImageWrapper, secondaryText, contentPanelImageVerticalWrapper, line,
+} = styles;
+
+const ContentPanel = ({
+  alt, heading, img, imageIsVertical, title, text, text2, reverseOrder,
+}) => (
+  <div className={`flexColumn ${contentPanel}`}>
+    <div className={contentPanelHeading}>
+      <h2 className={contentPanelTitle}>{title}</h2>
+    </div>
+    <div className={contentPanelBody} style={{ flexDirection: reverseOrder ? 'row-reverse' : 'row' }}>
+      <div className={contentPanelText} style={!reverseOrder ? { paddingRight: '20px' } : { paddingLeft: '20px' }}>
+        {heading && <h2 className={contentPanelTextTitle}>{heading}</h2>}
+        <p>{text}</p>
+        {text2 && (<p className={secondaryText}>{text2}</p>)}
+      </div>
+      <div className={!imageIsVertical ? contentPanelImageWrapper : contentPanelImageVerticalWrapper}>
+        <img className={contentPanelImage} src={img} alt={alt} />
+      </div>
+    </div>
+    <div className={line} />
+  </div>
+);
+
+ContentPanel.defaultProps = {
+  reverseOrder: false,
+  alt: '',
+  heading: '',
+  text2: '',
+  imageIsVertical: false,
+};
+
+ContentPanel.propTypes = {
+  alt: PropTypes.string,
+  heading: PropTypes.string,
+  imageIsVertical: PropTypes.bool,
+  img: PropTypes.string.isRequired,
+  reverseOrder: PropTypes.bool,
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  text2: PropTypes.string,
+};
+
+export default ContentPanel;
